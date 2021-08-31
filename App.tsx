c8ddcda5baba28f1281c25c560b313cd67e526db
Eric Chan <eric88ordinary@gmail.com>
2021-08-31 20:07:10 +0800

    add test draft

--- App.tsx
+++ App.tsx
@@ -1,14 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, Dimensions, StyleSheet, Text, View } from 'react-native';
+import { Swipeable, GestureEventPayload, PanGestureHandlerEventPayload } from 'react-native-gesture-handler';
 
-import Swiper from 'components/Swiper';
+import Swiper from 'components/swiper.component';
+import NativeCamera from 'components/camera.component.native';
 
+const { width, height } = Dimensions.get('screen');
 export default function App() {
+  const [openCamera, setOpenCamera] = useState<boolean>(false);
+
+  const handleSwipeLeft = (_: Readonly<GestureEventPayload & PanGestureHandlerEventPayload>) => {
+    // 'worklet';
+
+    setOpenCamera(true);
+  };
+
+  const handleSwipeRight = (_: Readonly<GestureEventPayload & PanGestureHandlerEventPayload>) => {
+    // 'worklet';
+
+    setOpenCamera(false);
+  };
+
+  const handleSwipeableLeft = () => {
+
+    setOpenCamera(true);
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-      <Swiper />
-      <StatusBar style="auto" />
+      {/* {openCamera && (
+        <Swiper onSwipeRight={handleSwipeRight}>
+        </Swiper>
+      )} */}
+      {/* <Camera style={styles.camera} /> */}
+      {/* <Swiper style={styles.camera}>
+        <NativeCamera />
+      </Swiper> */}
+      {openCamera && <NativeCamera />}
+      <Swipeable onSwipeableLeftOpen={handleSwipeableLeft} containerStyle={styles.swipeable} >
+        <Text style={StyleSheet.absoluteFill}>Hello</Text>
+      </Swipeable>
+      {/* <Swiper style={styles.text} onSwipeLeft={handleSwipeLeft}>
+        <Text style={StyleSheet.absoluteFill}>Hello</Text>
+      </Swiper> */}
+      {/* <Swiper onSwipeLeft={handleSwipeLeft} /> */}
+      <StatusBar style='auto' />
     </SafeAreaView>
   );
 }
@@ -16,6 +53,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     backgroundColor: 'black',
   },
+  swipeable: {
+    flex: 1,
+    backgroundColor: 'yellow',
+  },
+  camera: {
+    left: 0,
+    right: width,
+  },
+  text: {
+    left: 0,
+    right: width,
+  }
 });
